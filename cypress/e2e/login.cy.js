058f71d44e9ea47b03d32b3918299002409f50b1
Gabriel Logan <gabriellogan.0804@gmail.com>
2023-07-21 20:01:17 -0300

    teste

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,3 @@
-/// <reference types="cypress"/>
-
 context('Funcionalidade Login', () => {
     http://lojaebac.ebaconline.art.br/
 
--- cypress/e2e/produtos.cy.js
+++ cypress/e2e/produtos.cy.js
@@ -1,5 +1,3 @@
-/// <reference types="cypress" />
-
 describe('Funcionalidade Página de produtos', () => {
 
     beforeEach(() => {
